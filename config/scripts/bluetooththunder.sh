780e3d3fe3e3d6fa54a57ae0fb813d755ca64d25
MkKvcs <148012377+MkKvcs@users.noreply.github.com>
2024-05-13 13:20:32 +0200

    fix: drop-in instead of a script

--- config/scripts/bluetooththunder.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -oue pipefail
-BLUE_MOD_FILE="/etc/modprobe.d/99-bluetooththunder.conf"
-sh -c 'echo "install bluetooth /sbin/modprobe --ignore-install bluetooth" >> "$1"' _ "$BLUE_MOD_FILE"
-sh -c 'echo "install btusb /sbin/modprobe --ignore-install btusb" >> "$1"' _ "$BLUE_MOD_FILE"
-sh -c 'echo "install btusb /sbin/modprobe --ignore-install btusb" >> "$1"' _ "$BLUE_MOD_FILE"
-sh -c 'echo "install thunderbolt /sbin/modprobe --ignore-install thunderbolt" >> "$1"' _ "$BLUE_MOD_FILE"
-chmod 644 $BLUE_MOD_FILE
