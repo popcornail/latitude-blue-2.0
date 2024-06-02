27dbd81d6130c993305da5c22149422c6884028a
MkKvcs <148012377+MkKvcs@users.noreply.github.com>
2024-06-02 21:28:41 +0200

    Update chromium.sh

--- config/scripts/chromium.sh
+++ config/scripts/chromium.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -oue pipefail
-sed -i 's/--enable-features=/--enable-features=TouchpadOverscrollHistoryNavigation,/' /usr/etc/chromium/chromium.conf
+sed -i 's/--enable-features=/--enable-features=TouchpadOverscrollHistoryNavigation,/' /etc/chromium/chromium.conf
